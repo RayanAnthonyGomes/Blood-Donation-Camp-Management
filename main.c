c193ffab6029b38ed9a1dc8fb97753d5eb5478a8
RayanAnthonyGomes <rayangomes1329@gmail.com>
2025-08-15 20:37:53 +0600

    library files and constants

---
+++ main.c
@@ -0,0 +1,12 @@
+//Library files gula add kortesi
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//COnsstant function gula define kortesi
+#define MAX_SLOTS 5               
+#define STACK_SIZE 20             
+#define QUEUE_SIZE 20    
+
+
